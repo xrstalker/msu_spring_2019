3913e1a7f305608501488e2fe84cff24b2a0e615
Alexander Frolov <xrstalker@github.com>
2019-05-07 22:07:56 +0300

    [hw05] replace pointer with reference

--- 05/test.cpp
+++ 05/test.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 #include <thread>
 
-void proc(volatile int *x, int limit, int order)
+void proc(volatile int &x, int limit, int order)
 {
     limit -= 1 - order; 
-    while (*x < limit) {
-        if (*x % 2 == order) {
+    while (x < limit) {
+        if (x % 2 == order) {
             if (order)
                 std::cout << "pong" << std::endl;
             else
                 std::cout << "ping" << std::endl;
-            ++*x;
+            ++x;
         }
     }
 }
@@ -20,8 +20,8 @@ main()
 {
     int N = 1000000;
     volatile int x = 0;
-    std::thread t1 = std::thread(proc, &x, N, 0);
-    proc(&x, N, 1);
+    std::thread t1 = std::thread(proc, std::ref(x), N, 0);
+    proc(x, N, 1);
     t1.join();
     return 0; 
 }
